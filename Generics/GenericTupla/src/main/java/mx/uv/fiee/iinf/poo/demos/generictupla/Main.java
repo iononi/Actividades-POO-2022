a0e89355dab69b0c604239d6c85f8d3e91c255ee
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-02-12 14:06:39 -0600

    Agrega la lógica para trabajar con tuplas genéricas

--- Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Main.java
+++ Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Main.java
@@ -1,19 +1,18 @@
 package mx.uv.fiee.iinf.poo.demos.generictupla;
 
 public class Main {
-
+    
     public static void main (String [] args) {
-        GenericTupla<Integer, Integer> point1 = new GenericTupla<>(1, 5);
-        GenericTupla<Integer, Integer> point2 = new GenericTupla<>(1, 15);
-
-        boolean result = Utilities.compare (point1, point2);
-        System.out.println (result);
-
-        boolean result2 = Utilities.compare ("Hola", "Hola");
-        System.out.println (result2);
-
-        boolean result3 = Utilities.compare (3.0, 3.0);
-        System.out.println (result3);
+        GenericTupla<Integer, String> point1 = new GenericTupla<>(1, "Eduardo");
+        GenericTupla<Integer, String> point2 = new GenericTupla<>(2, "Eduardo");
+        ObjectTupla objTuple = new ObjectTupla(3, "Mimi");
+        boolean isEqual = Utilities.compare(point1, point2);
+        System.out.println(point1);
+        System.out.println(point2);
+        System.out.println(objTuple);
+        System.out.println("Checking tuples' equality using equals method: " + point1.equals(point2));
+        System.out.println("Checking tuples' equiality using Utilities.compare method: " + isEqual);
+        System.out.println("Comparing point1 and objTuple: " + Utilities.compare(point1, objTuple));
     }
 
 }
