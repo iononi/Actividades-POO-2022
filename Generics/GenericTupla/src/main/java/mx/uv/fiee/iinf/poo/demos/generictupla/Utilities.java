8d76094c8aec77eda770ee951ad06dd4345c7d15
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-02-12 14:07:10 -0600

    Clase con método estático para comparar tuplas genéricas

--- Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Utilities.java
+++ Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Utilities.java
@@ -1,9 +1,16 @@
 package mx.uv.fiee.iinf.poo.demos.generictupla;
 
+/**
+ * 
+ * @author Eduardo Ruiz Rios <eduardo.ruiz.rios09 at gmail.com>
+ */
 public class Utilities {
-
-    public static <T> boolean compare (T o1, T o2) {
-        return o1.equals (o2);
+    public static <T, U> boolean compare(T firstTuple, U secondTuple) {
+        if (firstTuple instanceof GenericTupla && secondTuple instanceof GenericTupla) {
+            GenericTupla myTuple = (GenericTupla) firstTuple;
+            GenericTupla anotherTuple = (GenericTupla) secondTuple;
+            return myTuple.getKey().equals(anotherTuple.getKey()) && myTuple.getValue().equals(anotherTuple.getValue());
+        }
+        return false;
     }
-
 }
