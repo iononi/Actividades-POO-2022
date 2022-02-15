77cff39fcf37b413b42a3742c2ca254bdd1e4237
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2022-02-14 22:57:42 -0600

    Compare parametrized method

--- Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Main.java
+++ Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Main.java
@@ -3,7 +3,17 @@ package mx.uv.fiee.iinf.poo.demos.generictupla;
 public class Main {
 
     public static void main (String [] args) {
+        GenericTupla<Integer, Integer> point1 = new GenericTupla<>(1, 5);
+        GenericTupla<Integer, Integer> point2 = new GenericTupla<>(1, 15);
 
+        boolean result = Utilities.compare (point1, point2);
+        System.out.println (result);
+
+        boolean result2 = Utilities.compare ("Hola", "Hola");
+        System.out.println (result2);
+
+        boolean result3 = Utilities.compare (3.0, 3.0);
+        System.out.println (result3);
     }
 
 }
---
+++ Generics/GenericTupla/src/main/java/mx/uv/fiee/iinf/poo/demos/generictupla/Utilities.java
@@ -0,0 +1,9 @@
+package mx.uv.fiee.iinf.poo.demos.generictupla;
+
+public class Utilities {
+
+    public static <T> boolean compare (T o1, T o2) {
+        return o1.equals (o2);
+    }
+
+}
