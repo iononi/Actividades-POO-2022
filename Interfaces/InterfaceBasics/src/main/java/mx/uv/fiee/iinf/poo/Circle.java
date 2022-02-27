63b19358b06091cfb35e13880204d1edad5a25d7
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-02-26 21:13:31 -0600

    Class to represent a circle object

--- Interfaces/InterfaceBasics/src/main/java/mx/uv/fiee/iinf/poo/Circle.java
+++ Interfaces/InterfaceBasics/src/main/java/mx/uv/fiee/iinf/poo/Circle.java
@@ -1,18 +1,35 @@
 package mx.uv.fiee.iinf.poo;
 
-public class Circle implements Draw, Calculate {
+public class Circle implements Draw, Calculate, Comparable<Circle> {
+
+    private final double radius;
+    private int fillColor;
+    private int drawColor;
+
+    public Circle(double radius, int fillColor, int drawColor) {
+        this.radius = radius;
+        this.fillColor = fillColor;
+        this.drawColor = drawColor;
+    }
+
+
     @Override
     public double calculateArea() {
-        return 0;
+        return Math.PI * Math.pow(radius, 2);
     }
 
     @Override
     public void draw(int color) {
-
+        drawColor = color;
     }
 
     @Override
     public void fill(int color) {
+        fillColor = color;
+    }
 
+    @Override
+    public int compareTo(Circle obj) {
+        return Double.compare(this.calculateArea(), obj.calculateArea());
     }
 }
