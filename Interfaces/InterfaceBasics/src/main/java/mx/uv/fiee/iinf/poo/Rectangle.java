08732e5d208e60d7183e5d1609d2a096df92a8c3
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-02-26 21:15:21 -0600

    Class to represent a rectangle

--- Interfaces/InterfaceBasics/src/main/java/mx/uv/fiee/iinf/poo/Rectangle.java
+++ Interfaces/InterfaceBasics/src/main/java/mx/uv/fiee/iinf/poo/Rectangle.java
@@ -1,18 +1,36 @@
 package mx.uv.fiee.iinf.poo;
 
-public class Rectangle implements Draw, Calculate {
+public class Rectangle implements Draw, Calculate, Comparable<Rectangle> {
+
+    private final double height;
+    private final double length;
+    private int fillColor;
+    private int drawColor;
+
+    public Rectangle(double height, double length, int fillColor, int drawColor) {
+        this.height = height;
+        this.length = length;
+        this.fillColor = fillColor;
+        this.drawColor = drawColor;
+    }
+
     @Override
     public double calculateArea() {
-        return 0;
+        return this.height * this.length;
     }
 
     @Override
     public void draw(int color) {
-
+        drawColor = color;
     }
 
     @Override
     public void fill(int color) {
+        fillColor = color;
+    }
 
+    @Override
+    public int compareTo(Rectangle obj) {
+        return Double.compare(this.calculateArea(), obj.calculateArea());
     }
 }
